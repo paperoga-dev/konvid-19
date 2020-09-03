78dc09ebe4def9cc981cef193f87f7b54e50c83b
Paperoga <paperoga.dev@gmail.com>
2020-09-04 00:13:56 +0200

    Change risk factor calculation model

--- konvid.js
+++ konvid.js
@@ -41,111 +41,172 @@ function createDataForCookie(dataDict) {
 }
 
 function update() {
+    var tm = null;
+    var daysToKillerContact = null;
+
+    var tu = null;
+    var daysToVictimContact = null;
+
     var killerHasFever = null;
     var R = null;
     var daysBetweenContactAndFever = null;
 
-    document.getElementsByName("btnKillerHasFever").forEach((btn) => { if (btn.checked) killerHasFever = parseFloat(btn.value); });
-    if (killerHasFever === 1.0) {
-        document.getElementById("slDaysBetweenContactAndFever").disabled = false;
+    var contactDuration = null;
+    var fp = null;
 
-        daysBetweenContactAndFever = 2.0 - document.getElementById("slDaysBetweenContactAndFever").valueAsNumber;
-        R = Math.exp((-Math.pow(-8.0 + daysBetweenContactAndFever, 2.0) / 18.0)) / (3.0 * Math.sqrt(2.0 * Math.PI)) / 0.1329805;
-    } else {
-        document.getElementById("slDaysBetweenContactAndFever").disabled = true;
-        R = 0.5;
-    }
-
-    var contactDuration = document.getElementById("slContactDuration").valueAsNumber;
-    var fp = ((Math.log(contactDuration + 1.0)) / (Math.log(61.0))) + 0.5;
-
-    var actorsDistance = document.getElementById("slActorsDistance").valueAsNumber;
-    var ne = Math.pow(1.0 - actorsDistance / 250.0, 2.0);
+    var actorsDistance = null;
+    var ne = null;
 
     var closedSpace = null;
-    document.getElementsByName("btnPlaceType").forEach((btn) => { if (btn.checked) closedSpace = parseFloat(btn.value); });
-    var fl = ((closedSpace === 1.0) ? 100.0 : 80.0) / 100.0;
+    var fl = null;
 
-    var killerShield = 0;
-    document.getElementsByName("btnKillerShieldType").forEach((btn) => { if (btn.checked) killerShield = parseFloat(btn.value); });
-    var fi = 1.0 - killerShield / 100.0;
+    var killerShield = null;
+    var fi = null;
 
-    var victimShield = 0;
-    document.getElementsByName("btnVictimShieldType").forEach((btn) => { if (btn.checked) victimShield = parseFloat(btn.value); });
-    var fc = 1.0 - victimShield / 100.0;
+    var victimShield = null;
+    var fc = null;
 
-    var victimAge = document.getElementById("slVictimAge").valueAsNumber;
-    var mL = null;
-    if ((victimAge >= 0) && (victimAge < 10))
-        mL = 0.0;
-    else if ((victimAge >= 10) && (victimAge < 20))
-        mL = 1.0;
-    else if ((victimAge >= 20) && (victimAge < 30))
-        mL = 2.0;
-    else if ((victimAge >= 30) && (victimAge < 40))
-        mL = 5.0;
-    else if ((victimAge >= 40) && (victimAge < 50))
-        mL = 12.0;
-    else if ((victimAge >= 50) && (victimAge < 60))
-        mL = 17.0;
-    else if ((victimAge >= 60) && (victimAge < 70))
-        mL = 56.0;
-    else if ((victimAge >= 70) && (victimAge < 80))
-        mL = 71.0;
-    else if ((victimAge >= 80) && (victimAge < 90))
-        mL = 85.0;
-    else if ((victimAge >= 90) && (victimAge < 100))
-        mL = 95.0;
-    else
-        mL = 100.0;
-
-    var L = (mL / 200.0) + 1.0;
-
-    var tu = null;
-    var daysToVictimContact = document.getElementById("slDaysToVictimContact").valueAsNumber;
-    if (((daysToVictimContact >= -15) && (daysToVictimContact <= -12)) ||
-        ((daysToVictimContact >= 3)))
-        tu = 1.0;
-    else
-        document.getElementsByName("btnKillerSwabState").forEach((btn) => { if (btn.checked) tu = parseFloat(btn.value); });
-
-    var tm = null;
-    document.getElementsByName("btnVictimSwabState").forEach((btn) => { if (btn.checked) tm = parseFloat(btn.value); });
-    if (tm != 1) {
-        document.getElementById("slDaysToKillerContact").disabled = false;
-        var daysToKillerContact = document.getElementById("slDaysToKillerContact").valueAsNumber;
-        if ((daysToKillerContact >= 0) && (daysToKillerContact <= 3))
-            tm = 1.0;
-        else
-            tm = 0.3;
-    } else {
-        document.getElementById("slDaysToKillerContact").disabled = true;
-    }
+    var victimAge = null;
+    var L = null;
 
     var illness1Severity = null;
-    document.getElementsByName("btnIllness1Severity").forEach((btn) => { if (btn.checked) illness1Severity = parseFloat(btn.value); });
     var illness2Severity = null;
-    document.getElementsByName("btnIllness2Severity").forEach((btn) => { if (btn.checked) illness2Severity = parseFloat(btn.value); });
     var illness3Severity = null;
-    document.getElementsByName("btnIllness3Severity").forEach((btn) => { if (btn.checked) illness3Severity = parseFloat(btn.value); });
 
-    if (illness1Severity < illness2Severity)
-        [illness1Severity, illness2Severity] = [illness2Severity, illness1Severity];
+    var P1 = null;
+    var P2 = null;
+    var P3 = null;
 
-    if (illness1Severity < illness3Severity)
-        [illness1Severity, illness3Severity] = [illness3Severity, illness1Severity];
+    var P = null;
 
-    if (illness2Severity < illness3Severity)
-        [illness2Severity, illness3Severity] = [illness3Severity, illness2Severity];
+    var realN = null;
 
-    var P1 = ((illness1Severity / 100.0) + 1.0);
-    var P2 = ((illness2Severity / 200.0) + 1.0);
-    var P3 = ((illness3Severity / 400.0) + 1.0);
+    try {
+        document.getElementsByName("btnVictimSwabState").forEach((btn) => { if (btn.checked) tm = parseFloat(btn.value); });
+        if (tm === 3.0) {
+            document.getElementById("slDaysToKillerContact").disabled = true;
+            tm = 1.0;
+        } else if (tm === 2.0) {
+            document.getElementById("slDaysToKillerContact").disabled = true;
+            throw 0;
+        } else if (tm === 1.0) {
+            document.getElementById("slDaysToKillerContact").disabled = true;
+            tm = 0.3;
+        } else {
+            document.getElementById("slDaysToKillerContact").disabled = false;
+            daysToKillerContact = document.getElementById("slDaysToKillerContact").valueAsNumber;
+            if ((daysToKillerContact >= 0) && (daysToKillerContact <= 3))
+                tm = 1.0;
+            else
+                tm = 0.3;
+        }
 
-    var P = (((((P1 * P2 * P3) - 1.0) / ((1.5 * 1.25 * 1.125) - 1.0)) * 0.5) + 1.0)
+        document.getElementsByName("btnKillerSwabState").forEach((btn) => { if (btn.checked) tu = parseFloat(btn.value); });
+        if (tu === 4) {
+            document.getElementById("slDaysToVictimContact").disabled = true;
+            tu = 1.0;
+        } else if (tu === 3) {
+            document.getElementById("slDaysToVictimContact").disabled = true;
+            tu = 1.1;
+        } else if (tu === 2) {
+            document.getElementById("slDaysToVictimContact").disabled = false;
+            daysToVictimContact = document.getElementById("slDaysToVictimContact").valueAsNumber;
+            if (((daysToVictimContact >= -15) && (daysToVictimContact <= -12)) ||
+                ((daysToVictimContact >= 3)))
+                tu = 1.0;
+            else
+                tu = 1.1;
+        } else if (tu === 1) {
+            document.getElementById("slDaysToVictimContact").disabled = true;
+            tu = 0.8;
+        } else {
+            document.getElementById("slDaysToVictimContact").disabled = false;
+            daysToVictimContact = document.getElementById("slDaysToVictimContact").valueAsNumber;
+            if (((daysToVictimContact >= -15) && (daysToVictimContact <= -12)) ||
+                ((daysToVictimContact >= 3)))
+                tu = 1.0;
+            else
+                tu = 0.8;
+        }
 
-    var realN = R * tu * tm * fp * ne * fl * fi * fc * L * P;
-    var N = Math.min(100, Math.round(realN * 100.0 / 5.0));
+        document.getElementsByName("btnKillerHasFever").forEach((btn) => { if (btn.checked) killerHasFever = parseFloat(btn.value); });
+        if (killerHasFever === 1.0) {
+            document.getElementById("slDaysBetweenContactAndFever").disabled = false;
+
+            daysBetweenContactAndFever = 2.0 - document.getElementById("slDaysBetweenContactAndFever").valueAsNumber;
+            R = Math.exp((-Math.pow(-8.0 + daysBetweenContactAndFever, 2.0) / 18.0)) / (3.0 * Math.sqrt(2.0 * Math.PI)) / 0.1329805;
+        } else {
+            document.getElementById("slDaysBetweenContactAndFever").disabled = true;
+            R = 0.5;
+        }
+
+        contactDuration = document.getElementById("slContactDuration").valueAsNumber;
+        fp = ((Math.log(contactDuration + 1.0)) / (Math.log(61.0))) * 0.3 + 0.8;
+
+        actorsDistance = document.getElementById("slActorsDistance").valueAsNumber;
+        ne = Math.pow(1.0 - actorsDistance / 300.0, 2.0);
+
+        document.getElementsByName("btnPlaceType").forEach((btn) => { if (btn.checked) closedSpace = parseFloat(btn.value); });
+        fl = ((closedSpace === 1.0) ? 100.0 : 90.0) / 100.0;
+
+        document.getElementsByName("btnKillerShieldType").forEach((btn) => { if (btn.checked) killerShield = parseFloat(btn.value); });
+        fi = 1.0 - killerShield / 100.0;
+
+        document.getElementsByName("btnVictimShieldType").forEach((btn) => { if (btn.checked) victimShield = parseFloat(btn.value); });
+        fc = 1.0 - victimShield / 100.0;
+
+        victimAge = document.getElementById("slVictimAge").valueAsNumber;
+        var mL = null;
+        if ((victimAge >= 0) && (victimAge < 10))
+            mL = 0.0;
+        else if ((victimAge >= 10) && (victimAge < 20))
+            mL = 1.0;
+        else if ((victimAge >= 20) && (victimAge < 30))
+            mL = 2.0;
+        else if ((victimAge >= 30) && (victimAge < 40))
+            mL = 5.0;
+        else if ((victimAge >= 40) && (victimAge < 50))
+            mL = 12.0;
+        else if ((victimAge >= 50) && (victimAge < 60))
+            mL = 17.0;
+        else if ((victimAge >= 60) && (victimAge < 70))
+            mL = 56.0;
+        else if ((victimAge >= 70) && (victimAge < 80))
+            mL = 71.0;
+        else if ((victimAge >= 80) && (victimAge < 90))
+            mL = 85.0;
+        else if ((victimAge >= 90) && (victimAge < 100))
+            mL = 95.0;
+        else
+            mL = 100.0;
+
+        L = (mL / 100.0) * 0.1 + 1.0;
+
+        document.getElementsByName("btnIllness1Severity").forEach((btn) => { if (btn.checked) illness1Severity = parseFloat(btn.value); });
+        document.getElementsByName("btnIllness2Severity").forEach((btn) => { if (btn.checked) illness2Severity = parseFloat(btn.value); });
+        document.getElementsByName("btnIllness3Severity").forEach((btn) => { if (btn.checked) illness3Severity = parseFloat(btn.value); });
+
+        if (illness1Severity < illness2Severity)
+            [illness1Severity, illness2Severity] = [illness2Severity, illness1Severity];
+
+        if (illness1Severity < illness3Severity)
+            [illness1Severity, illness3Severity] = [illness3Severity, illness1Severity];
+
+        if (illness2Severity < illness3Severity)
+            [illness2Severity, illness3Severity] = [illness3Severity, illness2Severity];
+
+        P1 = ((illness1Severity / 100.0) + 1.0);
+        P2 = ((illness2Severity / 200.0) + 1.0);
+        P3 = ((illness3Severity / 400.0) + 1.0);
+
+        P = (((((P1 * P2 * P3) - 1.0) / ((1.5 * 1.25 * 1.125) - 1.0)) * 0.1) + 1.0);
+
+        realN = R * tu * tm * fp * ne * fl * fi * fc * L * P;
+    } catch (exc) {
+        realN = 1.4641;
+    }
+
+    var N = Math.min(100, Math.round(realN * 100.0 / 1.4641));
 
     var progress = document.querySelector('.progress-done');
     progress.setAttribute('data-done', N);
